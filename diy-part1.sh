f7e8d9d335145f9b32979c459db146e74c3874a1
aliass532 <56620619+aliass532@users.noreply.github.com>
2026-02-02 20:11:24 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -13,7 +13,8 @@
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 sed -i 's/10.0.2.1/192.168.1.1/g' package/base-files/files/bin/config_generate
-sed -i 's/set=llvm.download-ci-llvm=true/set=llvm.download-ci-llvm=false/g' feeds/packages/lang/rust/Makefile
+sed -i 's/1.89.0/1.92.0/g' feeds/packages/lang/rust/Makefile
+sed -i 's/0b9d55610d8270e06c44f459d1e2b7918a5e673809c592abed9b9c600e33d95a/ebee170bfe4c4dfc59521a101de651e5534f4dae889756a5c97ca9ea40d0c307/g' feeds/packages/lang/rust/Makefile
 # Add a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
